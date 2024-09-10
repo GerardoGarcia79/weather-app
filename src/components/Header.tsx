733bf3390c80bd9b4361e9275dfb5f35d37013c4
Gerardo de Leon <gerardo-leon10@hotmail.com>
2024-09-09 18:13:16 -0600

    Styling header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,16 @@
+import { LuSearch } from "react-icons/lu";
+
 const Header = () => {
   return (
-    <div className="flex justify-center mb-10">
-      <input type="text" />
-      <button>Search</button>
+    <div className="flex justify-center">
+      <input
+        type="text"
+        placeholder="Enter a city..."
+        className="flex flex-grow rounded-full p-2 mr-3 bg-[#e8fcfc] placeholder:text-[#626262] text-lg focus:outline-2 focus:outline-white focus:outline-offset-1 transition-all ease-linear	duration-200 outline-[#462ac9] outline-offset-1 outline-2 outline"
+      />
+      <button className="bg-[#e8fcfc] rounded-full cursor-pointer p-4">
+        <LuSearch size="16px" />
+      </button>
     </div>
   );
 };
